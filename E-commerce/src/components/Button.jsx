56349fb6c2c1056538bc30ebd1fe946e65d1f9d7
Feyitee <anjuwontobi99@gmail.com>
2025-07-18 13:54:21 +0100

    the-hero-text-styling

---
+++ E-commerce/src/components/Button.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Button = ({ label, iconUrl }) => {
+  return (
+    <button
+      className="flex justify-center ite gap-2 px-7
+    py-4 border font-montserrat text-lg leading-none
+    bg-red-400 border-red-400 rounded-full text-white"
+    >
+      Click Me
+      <img
+        src={iconUrl}
+        alt="arrow right icon"
+        className="ml-2 rounded-full w-5 h-5"
+      />
+    </button>
+  );
+};
+
+export default Button;
--- E-commerce/src/sections/Hero.jsx
+++ E-commerce/src/sections/Hero.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import Button from "../components/Button";
+import { arrowRight } from "../assets/icons";
+import { statistics } from "../constants/index";
 const Hero = () => {
   return (
     <section
@@ -9,19 +12,45 @@ const Hero = () => {
     >
       <div
         className="relative xl:w-2/5 flex flex-col 
-      items-center justify-center w-full max-xl:padding-x pt-28"
+      items-start justify-center w-full max-xl:padding-x pt-28"
       >
-        <p>Our Summer Collection</p>
-        <h1>
-          <span>The New Arrival</span>
+        <p className="text-xl text-red-400 font-montserrat">
+          Our Summer Collection
+        </p>
+        <h1
+          className="mt-10 font-palanquin text-8xl 
+        max-sm:text-[72px] max-sm:leading-[82px] font-bold"
+        >
+          <span
+            className="xl:bg-white 
+          xl:whitespace-nowrap 
+          relative z-10 pr-10"
+          >
+            The New Arrival
+          </span>
           <br />
-          <span>Nike</span>
+          <span className="text-red-400 inline-block mt-3">Nike</span>
           Shoes
         </h1>
         <p>
           Discover stylish Nike arrival, quality comfort, and innovations from
           active life
         </p>
+        <Button label="Show right" iconUrl={arrowRight} />
+        <div
+          className="flex justify-starts 
+        items-start flex-wrap
+      w-full mt-20 gap-16"
+        >
+          {statistics.map((stat) => {
+            return (
+              <div key={stat.label}>
+                <p>{stat.value}</p>
+                <p>{stat.label}</p>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </section>
   );
