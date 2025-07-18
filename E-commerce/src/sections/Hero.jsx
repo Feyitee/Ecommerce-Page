8454e663d1e5b0d8994dc2603ba494046b3d0fdc
Feyitee <anjuwontobi99@gmail.com>
2025-07-18 11:35:59 +0100

    hero-arrivals

--- E-commerce/src/sections/Hero.jsx
+++ E-commerce/src/sections/Hero.jsx
@@ -3,10 +3,28 @@ const Hero = () => {
   return (
     <section
       id="home"
-      className="w-full flex flex-col xl:flex-row
-     border-red-500 border-2 min-h-screen 
-     justify-center gap-10 max-container p-2"
-    ></section>
+      className="w-full flex flex-col xl:flex-row min-h-screen
+     border-red-500 border-2
+     justify-center gap-10 max-container"
+    >
+      <div
+        className="relative xl:w-2/5 flex flex-col 
+      items-center justify-center w-full max-xl:padding-padding-x pt-28"
+      >
+        <p>Our Summer Collection</p>
+        <h1>
+          <span>The New Arrival</span>
+          <br />
+          <span>Nike</span>
+          Shoes
+        </h1>
+        <p>
+          {" "}
+          Discover stylish Nike arrival, quality comfort, and innovations from
+          active life
+        </p>
+      </div>
+    </section>
   );
 };
 
