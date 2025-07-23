1fed4c02c4f1b564ff2b765e3336d797709af00e
Feyitee <anjuwontobi99@gmail.com>
2025-07-23 11:56:23 +0100

    special-offers-skeleton

--- E-commerce/src/sections/Services.jsx
+++ E-commerce/src/sections/Services.jsx
@@ -4,13 +4,10 @@ import ServiceCard from "../components/ServiceCard";
 
 const Services = () => {
   return (
-    <section
-      className="max-container flex flex-wrap justify-center
-  items-center gap-9"
-    >
-      {services.map((service) => {
-        return <ServiceCard key={service.label} {...service} />;
-      })}
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
     </section>
   );
 };
